031ddb2a454f87eb9b82443308bafee7be9a8542
hmghaha <haha19ha@126.com>
2018-01-22 00:08:36 +0800

    complete the server

--- src/VerySimpleChatServer.java
+++ src/VerySimpleChatServer.java
@@ -2,6 +2,13 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+import javax.swing.text.StyledEditorKit.ItalicAction;
+
+import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
+import com.sun.org.apache.xpath.internal.operations.String;
+
+import sun.nio.cs.ext.ISCII91;
+
 public class VerySimpleChatServer {
 	
 	ArrayList clientOutputStreams;
@@ -9,8 +16,72 @@ public class VerySimpleChatServer {
 		BufferedReader reader;
 		Socket sock;
 		
-		public Client
+		public ClientHandler(Socket clientSocket){
+			try {
+				sock = clientSocket;
+				InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+				Reader = new BufferedReader(isReader);
+			}catch(Exception ex) {ex.printStackTrace();}
+		}
 		
+		public void run() {
+			String message;
+			try {
+				while((Message = Reader.readLine()) != null) {
+					System.out.println("reader " + message);
+					tellEveryone(message);
+				}
+			}catch(Exception ex) {ex.printStackTrace();}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		new VerySimpleChatServer().go();
+	}
+	
+	public void go() {
+		clientOutputStreams = new ArrayList();
+		try {
+			ServerSocket serverSocket = new ServerSocket(5000);
+			
+			while(true) {
+				Socket clientSocket = serverSocket.accept();
+				PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+				clientOutputStreams.add(writer);
+				
+				Thread thread = new Thread(new ClientHandler(clientSocket));
+				thread.start();
+				System.out.println("got a connection");
+			}
+		}catch(Exception ex) {ex.printStackTrace();}
+	}
+	
+	public void tellEveryone(String message) {
+		Iterator iterator  = clientOutputStreams.iterator();
+		while(it.hasNext()) {
+			try {
+				PrintWriter writer = (PrintWriter) it.next();
+				Writer.println(message);
+				Writer.flush();
+			}catch(Exception ex) {ex.printStackTrace();}
+		}
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
