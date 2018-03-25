a6c618506bd6869cf7865468f527951c06752119
hmghaha <haha19ha@126.com>
2018-03-25 23:16:26 +0800

    add

---
+++ Java/LearningJava/src/thread/InputOutputDemo.java
@@ -0,0 +1,22 @@
+package thread;
+
+class Res{
+	String name;
+	String sex;
+}
+
+class Input implements Runnable{
+	public void run() {
+		
+	}
+}
+
+class Output implements Runnable{
+	public void run() {
+		
+	}
+}
+
+public class InputOutputDemo {
+
+}
